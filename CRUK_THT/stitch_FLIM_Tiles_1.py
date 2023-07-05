9c432115fd38c34688a311de29d49ca6313b5977
arun.durai.p <47001187+duraiarun-p@users.noreply.github.com>
2023-07-05 21:06:40 +0100

    Image stitching - base code ready - tuning in progress

--- CRUK_THT/stitch_FLIM_Tiles_1.py
+++ CRUK_THT/stitch_FLIM_Tiles_1.py
@@ -43,6 +43,8 @@ lifetime_files = []
 
 positions = {}
 shifts = {}
+positions_new={}
+trunc_slice={}
 max_pos_1 = 0
 max_pos_2 = 0
 min_pos_1 = 10000
@@ -69,7 +71,7 @@ for line_num, line in enumerate(lines):
         x = round(float(position.split(",")[0]))
         y = round(float(position.split(",")[1]))
         positions[tile_index] = [x, y]
-        print('Positions: %s'%positions[tile_index])
+        # print('Positions pre shift: %s'%positions[tile_index])
         if max_pos_1 < x:
             max_pos_1 = x
         if max_pos_2 < y:
@@ -90,25 +92,27 @@ for line_num, line in enumerate(lines):
         print('Shift x , y: %s %s'%(shift_x,shift_y))
         shifts[tile_index] = [shift_x, shift_y]
 
-    for key, value in positions.items():
-        # positions[key] = [value[0] + shift_x, value[1] + shift_y]
+for key, value in positions.items():
+    print('Positions pre shift: %s'%positions[key])
+    shift_value=shifts[key]
+    positions_new[key] = [value[0] + shift_value[0], value[1] + shift_value[1]]
+    print('Positions pos shift: %s'%positions_new[key])
+    trunc_slice[key]=[shift_value[0],shift_value[1]]
+#     # positions[key] = [value[0], value[1]]
+#     sp = np.array( [value[0], value[1]])
+#     pos_ind=np.argwhere(sp<0)
+#     # sp[pos_ind]=0
+#     positions_new[key]=[sp[0] + shift_x, sp[1] + shift_y]
         
-        positions[key] = [value[0], value[1]]
+        
    
     
 no_of_chs=310
-# stitch_intensity = np.zeros([max_pos_2 + tile_size + shift_y, max_pos_1 + tile_size + shift_x], dtype=np.uint16)
-# stitch_intensity_arr = np.zeros([round(tile_size *3.5), round(tile_size*3.5)], dtype=np.uint16)
-# stitch_intensity = np.zeros([round(tile_size *3.5), round(tile_size*3.5)], dtype=np.uint16)
-# stitch_intensity_cube  = np.zeros([round(tile_size *3.5), round(tile_size*3.5), no_of_chs], dtype=np.uint16)
-
-# stitch_intensity = np.zeros([max_pos_2 + tile_size + shift_y, max_pos_1 + tile_size + shift_x], dtype=np.uint16)
-# stitch_intensity_cube = np.zeros([max_pos_2 + tile_size + shift_y, max_pos_1 + tile_size + shift_x,no_of_chs], dtype=np.uint16)
-
-
-stitch_intensity_arr = np.zeros([stitch_img_shape[0], stitch_img_shape[1]], dtype=np.uint16)
-stitch_intensity = np.zeros([stitch_img_shape[0], stitch_img_shape[1]], dtype=np.uint16)
-stitch_intensity_cube  = np.zeros([stitch_img_shape[0], stitch_img_shape[1], no_of_chs], dtype=np.uint16)
+# Axis must be swapped from Tile configuration to Python indexing
+stitch_intensity_arr = np.zeros([stitch_img_shape[1], stitch_img_shape[0]], dtype=np.uint16)
+stitch_intensity = np.zeros([stitch_img_shape[1], stitch_img_shape[0]], dtype=np.uint16)
+stitch_intensity_cube  = np.zeros([stitch_img_shape[1], stitch_img_shape[0], no_of_chs], dtype=np.uint16)
+stitch_flt_cube  = np.zeros([stitch_img_shape[1], stitch_img_shape[0], no_of_chs], dtype=np.uint16)
 
 # stitch_intensity_arr = np.zeros([stitch_img_shape[0] + shift_x, stitch_img_shape[1] + shift_y], dtype=np.uint16)
 # stitch_intensity = np.zeros([stitch_img_shape[0] + shift_x, stitch_img_shape[1] + shift_y], dtype=np.uint16)
@@ -121,13 +125,19 @@ stitch_intensity_cube  = np.zeros([stitch_img_shape[0], stitch_img_shape[1], no_
 
 #%%
 
-mat_file_order=np.array([1,4,7,2,5,8,3,6,9])
+img_file_order=np.array([1,4,7,2,5,8,3,6,9])
+mat_file_order=np.array([1,2,3,4,5,6,7,8,9])
 
-for t_i, p in positions.items():
+# for t_i, p in positions.items():
+for t_i, p in positions_new.items():    
+    
+    num=int(t_i)
+    print('Tile %s'%num)
     
     # print('%s %s'%(t_i,p))
     # print(f"{base_dir}/{t_i}.tiff")
-    intensity1 = cv2.imread(f"{base_dir}/{t_i}.tiff",-1)
+    img_file_order_index_str=str(img_file_order[int(t_i)-1])
+    intensity1 = cv2.imread(f"{base_dir}/{img_file_order_index_str}.tiff",-1)
     
     # mask = (intensity>0).astype(int)
     # if np.sum(mask) < 0.05*intensity.shape[0]*intensity.shape[1]:
@@ -135,52 +145,85 @@ for t_i, p in positions.items():
     
     # print('%s'%p)
     p=np.array(p)
-    p1=np.zeros_like(p)
-    p1[0]=p[0]+shifts[t_i][0]
-    p1[1]=p[1]+shifts[t_i][1]
+    # p1=p
+    # p1=np.zeros_like(p)
+    # p1[0]=p[0]+shifts[t_i][0]
+    # p1[1]=p[1]+shifts[t_i][1]
     print('S P %s'%p)
-    print('E P %s'%p1)
+    
     # p_siz=p1-p
     # print(p_siz)
     # print(intensity1.shape)
     # print(p,p1)
-    pos_ind=np.squeeze(np.argwhere(p<0))
+    # pos_ind=np.squeeze(np.argwhere(p<0))
+    # pos_ind=(np.argwhere(p<0))
+    # if len(pos_ind)==0:
+    #     print('No neg')
+    # else:
+    #     print(pos_ind)
+    #     print(len(pos_ind))
     # print(pos_ind)
+    
+    # if len(pos_ind)>0:
+        
     
     # stitch_intensity[p[1]:p[1]+tile_size, p[0]:p[0]+tile_size] = intensity1
     # stitch_intensity_arr[p[1]:p[1]+tile_size, p[0]:p[0]+tile_size] = intensity1
     
-    # stitch_intensity_arr[p[0]:p[0]+tile_size, p[1]:p[1]+tile_size] = intensity1
+    shift_value=shifts[t_i]   
+    p1=p+tile_size-shift_value
+    print('E P %s'%p1)
+    # stitch_intensity_arr[p[0]:p[0]+tile_size-shift_value[0], p[1]:p[1]+tile_size-shift_value[1]] = intensity1[shift_value[0]:,shift_value[1]:]
+    intensity_blk=intensity1[shift_value[0]:tile_size,shift_value[1]:tile_size]
+    intensity_blk_shape=np.array(intensity_blk.shape)
+    print('Img Block size: %s'%(intensity_blk_shape))
     
-    # stitch_intensity_arr[p1[0]:p1[0]+tile_size, p1[1]:p1[1]+tile_size] = intensity1
-    # stitch_intensity_arr[p1[0]:p1[0]+512, p1[1]:p1[1]+512] = intensity1
+    stitch_intensity_arr[p[0]:p1[0], p[1]:p1[1]] = intensity_blk
+    # stitch_intensity_arr[p[0]:p1[0], p[1]:p1[1]] = (stitch_intensity_arr[p[0]:p1[0], p[1]:p1[1]]+intensity_blk)*0.5
+    # img_stack=np.stack([stitch_intensity_arr[p[0]:p1[0], p[1]:p1[1]],intensity_blk])
+    # intensity_blk_med=np.median(img_stack,axis=0)
+    # stitch_intensity_arr[p[0]:p1[0], p[1]:p1[1]] = intensity_blk_med
     
-    # shift_ind1=shifts[t_i][0]
-    # shift_ind2=shifts[t_i][1]
-    # stitch_intensity_arr[p[1]:p[1]+tile_size, p[0]:p[0]+tile_size] = intensity1[shift_ind2:,-shift_ind1:]
-    
-    # stitch_intensity[p[1]:p1[1], p[0]:p1[0]] = intensity
-    # stitch_intensity[p[1]:p1[1], p[0]:p1[0]] = intensity[p1[1]-tile_size:,p1[0]-tile_size:]
-    # stitch_intensity[p[1]:p1[1], p[0]:p1[0]] = (intensity + stitch_intensity[p[1]:p1[1], p[0]:p1[0]])*0.5
-    # # intensity1=intensity[p[0]:p[0]+tile_size, p[1]:p[1]+tile_size]
-    # num=int(t_i)
-    # print('Tile %s'%num)
-    # plt.figure(2)
-    # plt.subplot(3,3,num)
-    # plt.imshow(intensity,cmap='gray')
-    # plt.show()
     
     mat_file_order_index_str=str(mat_file_order[int(t_i)-1])
     mat_contents=h5py.File(f"{base_dir}/{mat_file_order_index_str}.mat",'r+')
     # mat_contents=h5py.File(f"{base_dir}/{t_i}.mat",'r+')
+    
     allIntensityImages1_ref=mat_contents['allIntensityImages1']
-    allIntensityImages1=allIntensityImages1_ref[()]
+    allIntensityImages1=allIntensityImages1_ref[()]    
+    
+    intensity_blk1=allIntensityImages1[shift_value[0]:tile_size,shift_value[1]:tile_size]
+    intensity_blk1_shape=np.array(intensity_blk1.shape)
+    print('Mat Block size: %s'%(intensity_blk1_shape))
+    
+    stitch_intensity[p[0]:p1[0], p[1]:p1[1]] = intensity_blk1
+    # stitch_intensity[p[0]:p1[0], p[1]:p1[1]] = (stitch_intensity[p[0]:p1[0], p[1]:p1[1]]+intensity_blk1)*0.5
+    
+    # img_stack_int=np.stack([stitch_intensity[p[0]:p1[0], p[1]:p1[1]],intensity_blk])
+    # intensity_blk_med_1=np.median(img_stack_int,axis=0)
+    # stitch_intensity[p[0]:p1[0], p[1]:p1[1]] = intensity_blk_med_1
+    
+    lifetimeAlphaData1_ref=mat_contents['lifetimeAlphaData1']
+    lifetimeAlphaData1=lifetimeAlphaData1_ref[()]
+    
+    intensity_blk1_cube=lifetimeAlphaData1[shift_value[0]:tile_size,shift_value[1]:tile_size,:]
+    intensity_blk1_cube_shape=np.array(intensity_blk1_cube.shape)
+    print('Mat Cube size: %s'%(intensity_blk1_cube_shape))
+    
+    stitch_intensity_cube[p[0]:p1[0], p[1]:p1[1],:] = intensity_blk1_cube
+    # stitch_intensity_cube[p[0]:p1[0], p[1]:p1[1],:] = (stitch_intensity_cube[p[0]:p1[0], p[1]:p1[1],:]+intensity_blk1_cube)*0.5
 
-    # lifetimeAlphaData1_ref=mat_contents['lifetimeAlphaData1']
-    # lifetimeAlphaData1=lifetimeAlphaData1_ref[()]
+    
 
-    # lifetimeImageData1_ref=mat_contents['lifetimeImageData1']
-    # lifetimeImageData1=lifetimeImageData1_ref[()]
+    lifetimeImageData1_ref=mat_contents['lifetimeImageData1']
+    lifetimeImageData1=lifetimeImageData1_ref[()]
+    
+    flt_blk1_cube=lifetimeImageData1[shift_value[0]:tile_size,shift_value[1]:tile_size,:]
+    flt_blk1_cube_shape=np.array(flt_blk1_cube.shape)
+    print('Mat Cube size: %s'%(flt_blk1_cube_shape))
+    
+    stitch_flt_cube[p[0]:p1[0], p[1]:p1[1],:] = flt_blk1_cube
+    
     
     # stitch_intensity_cube[p[1]:p[1]+tile_size, p[0]:p[0]+tile_size, :] = lifetimeAlphaData1
     
@@ -198,8 +241,8 @@ for t_i, p in positions.items():
     # stitch_intensity[p[0]:p[0]+tile_size, p[1]:p[1]+tile_size] = np.flipud(ndi.rotate(allIntensityImages1,90))
     # stitch_intensity_cube[p[0]:p[0]+tile_size, p[1]:p[1]+tile_size, :] = np.flipud(ndi.rotate(lifetimeAlphaData1,90))
     
-    num=int(t_i)
-    print('Tile %s'%num)
+    # num=int(t_i)
+    print('Tile finished %s'%num)
     plt.figure(2)
     plt.subplot(3,3,num)
     plt.imshow(intensity1,cmap='gray')
@@ -208,38 +251,43 @@ for t_i, p in positions.items():
     
 #%%
 
-stitch_intensity=np.flipud(ndi.rotate(stitch_intensity,90))
+# stitch_intensity=np.flipud(ndi.rotate(stitch_intensity,90))
 # stitch_intensity_cube=ndi.rotate(stitch_intensity_cube, 90)
 
-# stitch_intensity = cv2.rotate(stitch_intensity, cv2.ROTATE_90_CLOCKWISE)
+stitch_intensity = np.flipud(cv2.rotate(stitch_intensity, cv2.ROTATE_90_COUNTERCLOCKWISE))
 
 #%%
 
-# plt.figure(1)
-# plt.tight_layout()
-# plt.subplot(1,2,2)
-# plt.imshow(stitch_intensity, cmap="gray")
-# plt.title('from mat array')
-# plt.subplot(1,2,1)
-# plt.imshow(stitch_intensity_arr, cmap="gray")
-# plt.title('from img')
+plt.figure(1)
+plt.tight_layout()
+plt.subplot(1,2,2)
+plt.imshow(stitch_intensity, cmap="gray")
+plt.title('from mat array')
+plt.subplot(1,2,1)
+plt.imshow(stitch_intensity_arr, cmap="gray")
+plt.title('from img')
 
-# #%%
-# page=25
-# # plt.figure(3)
-# # plt.imshow(stitch_intensity_cube[:,:,page],cmap='gray')
-# # plt.show()
-# #%%
+#%%
+page=100
+plt.figure(3)
+plt.subplot(1,2,1)
+plt.imshow(stitch_intensity_cube[:,:,page],cmap='gray')
+plt.colorbar()
+plt.subplot(1,2,2)
+plt.imshow(stitch_flt_cube[:,:,page],cmap='gray')
+plt.colorbar()
+plt.show()
+#%%
 
 # plt.figure(4)
 # plt.imshow(stitch_fiji,cmap='gray')
 # plt.show()
-# #%%
-# plt.figure(5)
-# plt.subplot(1,2,1)
-# plt.imshow(stitch_fiji)
-# plt.title('Fiji Output')
-# plt.subplot(1,2,2)
-# plt.imshow(stitch_intensity,cmap='gray')
-# plt.title('Mat stitched')
-# plt.show()
+#%%
+plt.figure(5)
+plt.subplot(1,2,1)
+plt.imshow(stitch_fiji)
+plt.title('Fiji Output')
+plt.subplot(1,2,2)
+plt.imshow(stitch_intensity,cmap='gray')
+plt.title('Mat stitched')
+plt.show()
