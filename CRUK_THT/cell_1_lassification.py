b3fd4912649598cc6bf1eaf33065bb172edd6ee3
arun.durai.p <47001187+duraiarun-p@users.noreply.github.com>
2023-07-21 12:26:35 +0100

    Cell classification - ground truth - pixel coordinate transform - in progress

--- CRUK_THT/cell_1_lassification.py
+++ CRUK_THT/cell_1_lassification.py
@@ -9,7 +9,9 @@ import os
 import sys
 import csv
 import pandas as pd
-
+import cv2
+import matplotlib.pyplot as plt
+import matplotlib.patches as pltpatch
 #%%
 base_dir='/home/cruk/Documents/PyWS_CRUK/CRUK_Image_Analysis/Test_Data/Tumour_1/Row-1_Col-2_20230215/Mat_output'
 # base_dir='/home/cruk/Documents/PyWS_CRUK/CRUK_Image_Analysis/Test_Data/Tumour_1/Row-1_Col-9_20230222/Mat_output'
@@ -27,7 +29,7 @@ if os.path.exists(path_to_class_file)==False:
 #%% Extracting information from QuPATH classification output
 with open(path_to_class_file) as class_file:
     lines = [line.rstrip('\n') for line in class_file]
-
+# using tab as a separator
 items = [item.split('\t') for item in lines]
 
 del lines
@@ -35,3 +37,68 @@ del lines
 column_names=items[0] # Extracting table column names
 
 del items[0]
+# Convert numeric values into float datatype
+for item_idx in range(len(items)):
+    item=items[item_idx]
+    for ele_idx in range(len(item)):
+        ele=item[ele_idx]
+        try:
+            item[ele_idx]=float(ele)
+        except ValueError:
+            item[ele_idx]=ele
+    items[item_idx]=item
+
+#%% Extracting cell centroid from the QuPath output file
+
+# # Index of column in items list in order
+
+cell_centroid_x_ind=column_names.index("Centroid X px")
+cell_centroid_y_ind=column_names.index("Centroid Y px")
+cell_class=column_names.index("Class")
+cell_roi=column_names.index("ROI")
+cell_area=column_names.index("Cell: Area")
+cell_perimeter=column_names.index("Cell: Perimeter")
+cell_caliper_max=column_names.index("Cell: Max caliper")
+cell_caliper_min=column_names.index("Cell: Min caliper")
+
+
+cell_items=[]
+for item_idx in range(len(items)):
+    # Bounding box x co-ordinates
+    bound_x=[items[item_idx][cell_centroid_x_ind]+(items[item_idx][cell_caliper_max])*0.5,
+              items[item_idx][cell_centroid_x_ind]-(items[item_idx][cell_caliper_max])*0.5,
+              items[item_idx][cell_centroid_x_ind]+(items[item_idx][cell_caliper_min])*0.5,
+              items[item_idx][cell_centroid_x_ind]-(items[item_idx][cell_caliper_min])*0.5
+                ]
+    bound_x=[min(bound_x),max(bound_x)]
+    
+    bound_y=[items[item_idx][cell_centroid_y_ind]+(items[item_idx][cell_caliper_max])*0.5,
+              items[item_idx][cell_centroid_y_ind]-(items[item_idx][cell_caliper_max])*0.5,
+              items[item_idx][cell_centroid_y_ind]+(items[item_idx][cell_caliper_min])*0.5,
+              items[item_idx][cell_centroid_y_ind]-(items[item_idx][cell_caliper_min])*0.5
+                ]
+    bound_y=[min(bound_y),max(bound_y)]
+    
+    bound_area=(bound_x[1]-bound_x[0])*(bound_y[1]-bound_y[0])
+    
+    cell_item=[items[item_idx][cell_centroid_x_ind],items[item_idx][cell_centroid_y_ind],
+                items[item_idx][cell_class],items[item_idx][cell_roi],items[item_idx][cell_area],items[item_idx][cell_perimeter],
+                items[item_idx][cell_caliper_max],items[item_idx][cell_caliper_min],bound_x,bound_y,bound_area]
+    cell_items.append(cell_item)
+#%% Validating bounding box by overlaying them on original figure
+
+file_extension_type = ('.tif',) # , '.exe', 'jpg', '...')
+for hist_file in os.listdir(base_dir):
+    if hist_file.endswith(file_extension_type) and hist_file.startswith('R'):
+        print("Found a file {}".format(hist_file)) 
+        hist_img=cv2.imread(f"{base_dir}/{hist_file}")
+        
+
+#%% 
+
+plt.figure(1),
+plt.plot(cell_item[0],cell_item[1],marker='v', color="white")
+xy=(cell_item[-2],cell_item[-3])
+pltpatch.Rectangle(xy, 10, 10,linewidth=1, edgecolor='w', facecolor='none')
+plt.imshow(hist_img)
+
