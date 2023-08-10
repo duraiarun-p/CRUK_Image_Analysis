dc3adba67e01dd889b80fc682ccc174a9ce745fc
arun.durai.p <47001187+duraiarun-p@users.noreply.github.com>
2023-08-10 17:57:50 +0100

    Cell classification - PCA - in progress

--- CRUK_THT/Matlab_Test_Run/cell_1_classify_mat.m
+++ CRUK_THT/Matlab_Test_Run/cell_1_classify_mat.m
@@ -21,8 +21,8 @@ load("core_stitched_masked.mat")
 % u8_int=uint8(zeros(size(stitch_intensity_cube_masked)));
 flt_int=uint8(zeros(size(stitch_intensity_cube_masked)));
 flt_siz=size(stitch_intensity_cube_masked);
-spec=38;
-% for spec=1:flt_siz(3)
+% spec=38;
+for spec=1:flt_siz(3)
 slice_flt=stitch_flt_cube_masked(:,:,spec);
 slice_int=stitch_intensity_cube_masked(:,:,spec);
 % slice_flt_u8=uint8(255*mat2gray(slice_flt));
@@ -32,7 +32,7 @@ slice_int_u8=(1*mat2gray(slice_int));
 
 [new_slice, new_d]=imfuse(slice_flt_u8,slice_int_u8,'blend');
 flt_int(:,:,spec)=new_slice;
-% end
+end
 
 %%
 
@@ -40,9 +40,12 @@ figure(7),
 imshow(HE)
 %%
 nbin=256;
+nopcs=50;
+new_cell_width=20;
 cell_ind=1;
 noofcells=length(bound_txed);
 feat_matrix=zeros(nbin*flt_siz(3),noofcells);
+feat_matrix=zeros(nopcs*new_cell_width*new_cell_width,noofcells);
 for cell_ind=1:noofcells
 % for cell_ind=1:1
 row_start=floor(bound_txed(cell_ind,1));
@@ -50,12 +53,24 @@ row_stop=floor(bound_txed(cell_ind,3));
 col_start=floor(bound_txed(cell_ind,2));
 col_stop=floor(bound_txed(cell_ind,4));
 cell_box=flt_int(row_start:row_stop,col_start:col_stop,:);
+cell_box=imresize3(cell_box,[new_cell_width,new_cell_width,flt_siz(3)]);
+cell_size=size(cell_box);
+% disp(cell_size);
+listOfpixelValues=double(reshape(cell_box,cell_size(1)*cell_size(2),cell_size(3)));
+
+% listOfpixelValues=double(reshape(cell_box,cell_size(3),cell_size(1)*cell_size(2)));
+
+[coeff, score, latent, explained, mu] = pca(listOfpixelValues);
+% transformedImagePixelList = listOfpixelValues * coeff;
 % spec=100;
-feat=zeros(nbin,flt_siz(3));
-for spec=1:flt_siz(3)
-[counts,~] = imhist(cell_box(:,:,spec),nbin);
-feat(:,spec)=counts;
-end
+
+% [eigvec_St1,eigval_St1]=eig_decomp(cell_box(:,:,spec));
+% feat=zeros(nbin,flt_siz(3));
+feat=score(:,1:nopcs);
+% for spec=1:flt_siz(3)
+% [counts,~] = imhist(cell_box(:,:,spec),nbin);
+% feat(:,spec)=counts;
+% end
 feat_vector=rescale(feat(:));
 feat_matrix(:,cell_ind)=feat_vector;
 % figure(10);contour(feat);
@@ -63,6 +78,7 @@ feat_matrix(:,cell_ind)=feat_vector;
 % title(num2str(cell_ind));
 % pause(0.001);
 end
+%%
 %% Prepare Dataset
 feat_matrix=feat_matrix';
 class_data_org=table(feat_matrix,class_grnd_trth);
@@ -97,3 +113,16 @@ feat_matrix1=[feat_1;feat_2;feat_3];
 class_grnd_trth1=[class_1;class_2;class_3];
 % 
 class_data_1=table(feat_matrix1,class_grnd_trth1);
+
+
+
+
+
+%%
+function [eigvec_St1,eigval_St1]=eig_decomp(St1)
+[eigvec,eigval]=eig(double(St1));
+eigval=abs(diag(eigval)');		
+[eigval,I]=sort(eigval);
+eigval_St1=fliplr(eigval); 
+eigvec_St1=fliplr(eigvec(:,I));
+end
